3b89e4a7764ad81af54b0bee03a4bfdefbbfc8c7
Daniel Schlomer <djschlomer@gmail.com>
2015-10-16 22:46:28 -0500

    Plot 1

--- 03 Visualizations/Plot1.R
+++ 03 Visualizations/Plot1.R
@@ -7,17 +7,16 @@ df1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?qu
 
 df2 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from cbsa"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_djs3287', PASS='orcl_djs3287', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-head(df1)
-head(df2)
+df3 <- inner_join(df1, df2, by = "URBAN_AREA") %>% filter(POP_GROUP %in% c("Sml", "Med", "Lrg"))
 
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
-  labs(title='Traffic') +
-  labs(x="Year", y=paste("Commuters")) +
-  layer(data=df, 
-        mapping=aes(x=as.numeric(as.character(YR)), y=as.numeric(as.character(COMMUTERS)), color=POP_GROUP), 
+  labs(title='Urban Area Migration and Delay') +
+  labs(x="Net Migration / Total Population", y=paste("Delay Per Commuter")) +
+  layer(data=df3, 
+        mapping=aes(x=as.numeric(as.character(NETMIG/POPULATION)), y=as.numeric(as.character(DELAY_PER_COMMUTER)), color=POP_GROUP), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
