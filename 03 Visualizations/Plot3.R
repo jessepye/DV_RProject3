ed12d3e9020b67ed92213cbb998562491607c2ad
Jesse Pye <jessepye@gmail.com>
2015-10-16 23:41:51 -0500

    Finished Proj3.Rmd

--- 03 Visualizations/Plot3.R
+++ 03 Visualizations/Plot3.R
@@ -2,6 +2,7 @@ require("jsonlite")
 require("RCurl")
 require("extrafont")
 require("ggplot2")
+require("dplyr")
 
 df1 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from trafficshort"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_djs3287', PASS='orcl_djs3287', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
